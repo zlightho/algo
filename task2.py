60ee9910abe55b0a01c1d4206c0f1888a74bc966
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2023-01-10 22:09:35 +0300

    3d tests

--- task2.py
+++ task2.py
@@ -1,10 +1,15 @@
 from typing import List
 
-
 def odometer(oksana: List[int]) -> int:
-    """Get list with numbers, and return sum all elements with odd indices in the list"""
-    dist = 0
-    for i in range(len(oksana)):
-        if i % 2 == 0:
-            dist += oksana[i]
-    return dist
+    """Get list with speeds and durations, and return sum of all traveled distance."""
+    current_speed = oksana[0]
+    distance = current_speed * oksana[1]
+    if len(oksana) > 2:
+        for i in range(2, len(oksana), 2):
+            current_speed = oksana[i]
+            current_total_time = oksana[i+1]
+            movement_time = current_total_time - oksana[i-1]
+            distance = distance + current_speed * movement_time
+    return distance
+
+print(odometer([20, 2, 30, 6, 10, 7])) # 170
