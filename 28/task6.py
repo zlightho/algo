3bb5bbb0eac8fee2eeab9c5a3c082cd63022cc64
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-02-08 18:08:07 +0300

    task6.py

---
+++ 28/task6.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+def MassVote(N: int, Votes: List[int]) -> str:
+    """Receives the input number of candidates N >= 1 and an array containing N
+    votes cast for the corresponding candidates.
+    At the output, a line with the winner is formed.
+    """
+    total_votes = sum(Votes)
+    max_votes = max(Votes)
+    if max_votes / total_votes > 0.5:
+        return f"majority winner {Votes.index(max_votes) + 1}"
+    elif max_votes * 2 > total_votes:
+        return f"minority winner {Votes.index(max_votes) + 1}"
+    else:
+        max_vote_count = Votes.count(max_votes)
+        if max_vote_count == 1:
+            return f"minority winner {Votes.index(max_votes) + 1}"
+        else:
+            return "no winner"
+
+
+print(MassVote(5, [60, 10, 10, 15, 5]))
+print(MassVote(3, [10, 15, 10]))
+print(MassVote(4, [111, 111, 110, 110]))
