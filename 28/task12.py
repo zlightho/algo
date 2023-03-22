0bae7507afbe35baf4402665e69fc78e9e3960dd
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-03-22 17:55:03 +0300

    review

--- 28/task12.py
+++ 28/task12.py
@@ -3,17 +3,18 @@ from typing import List
 
 def MisterRobot(N: int, data: List[int]) -> bool:
     """
-    This function checks if it is possible to sort a given list of integers from 1 to N
-    using the following operation:
-    Take any three consecutive elements in the list, and rotate them to the left any number
-    of times.
+    This function checks if it is possible to sort a given list of integers
+    from 1 to N using the following operation:
+    Take any three consecutive elements in the list, and rotate them to the
+    left any number of times.
 
     Args:
-    N (int): The size of the list, and also the range of integers from 1 to N (inclusive).
-    data (List[int]): The input list of integers from 1 to N.
+    N (int): The size of the list, and also the range of integers from 1 to N
+    (inclusive). data (List[int]): The input list of integers from 1 to N.
 
     Returns:
-    bool: True if it is possible to sort the list using the specified operation, False otherwise.
+    bool: True if it`s possible to sort the list using the specified operation,
+    False otherwise.
     """
 
     for _ in range(N):
@@ -25,8 +26,4 @@ def MisterRobot(N: int, data: List[int]) -> bool:
                     data[i],
                 )
 
-    for i in range(N - 1):
-        if data[i] > data[i + 1]:
-            return False
-
-    return True
+    return all(data[i] <= data[i + 1] for i in range(N - 1))
