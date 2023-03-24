7e8ef38d0da5018c877648128039a64a81ae6536
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-03-24 14:51:42 +0300

    task13.py

---
+++ 28/task13.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def ShopOLAP(N: int, items: List[str]) -> dict:
+    """
+    receives N >= 1 rows about products in the above format as input,
+    and outputs an array of length M <= N containing a sales summary
+    in grouped form.
+    """
+    result_dict = {}
+    for i in items:
+        split_list = i.split()
+        key, value = split_list[0], int(split_list[1])
+        if key in result_dict:
+            result_dict[key] += value
+        else:
+            result_dict[key] = value
+    sorted_result_list = sorted(
+        result_dict.items(), key=lambda x: x[1], reverse=True
+    )
+    formatted_result = [f"{key} {value}" for key, value in sorted_result_list]
+    return formatted_result
