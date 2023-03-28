10afc82a4ac48e64fe2a77843629a2d921240b62
Artem Mikhaylov <artemmikhaylov6@gmail.com>
2023-03-28 17:52:45 +0300

    task14

--- 28/task14.py
+++ 28/task14.py
@@ -1,21 +1,81 @@
-operations_history  = ['']
-def BastShoe(command:str) -> str:
-    operation_and_value = command.split(' ',1)
-    operation, value = operation_and_value[0], operation_and_value[1]
-    global operations_history
-    if operation == '1':
-        operations_history.append((operations_history[-1] + value))
-    
-    elif operation == '2':
-        operations_history[-1] = operations_history[-1][:len(operations_history[-1])-(int(value))]
-    elif operation == '3':
-        if value >= len(operations_history[-1]):
-            return ''
-        return operations_history[-1][value]
+list_undo = []
+list_redo = []
+current_str = ""
+last_was_undo = False
+
+
+def append(cmd: str) -> str:
+
+    global current_str, last_was_undo, list_undo, list_redo
+
+    if not last_was_undo:
+        list_undo.append(current_str)
     else:
-        return ''
-    return operations_history[-1]
-print(BastShoe('1 Привет'))
-print(BastShoe('1  , Мир!'))
-print(BastShoe('1 ++'))
-print(BastShoe('2 2'))
+        list_redo = []
+        list_undo = [current_str]
+
+    last_was_undo = False
+    current_str += cmd
+    return current_str
+
+
+def delete(cmd: str) -> str:
+
+    n = int(cmd)
+
+    global current_str, last_was_undo, list_undo, list_redo
+
+    if not last_was_undo:
+        list_undo.append(current_str)
+    else:
+        list_redo = []
+        list_undo = [current_str]
+
+    last_was_undo = False
+    current_str = current_str[:-n]
+    return current_str
+
+
+def get(cmd: str) -> str:
+
+    i = int(cmd)
+    return current_str[
+        i : i + 1
+    ]  # берем срез чтобы не обрабатывать исключение
+
+
+def undo(cmd: str) -> str:
+
+    global current_str, last_was_undo
+
+    if not list_undo:
+        return current_str
+
+    last_was_undo = True
+
+    list_redo.append(current_str)
+    current_str = list_undo.pop()
+    return current_str
+
+
+def redo(cmd: str) -> str:
+
+    global current_str, last_was_undo
+
+    if not list_redo:
+        return current_str
+
+    last_was_undo = False
+
+    list_undo.append(current_str)
+    current_str = list_redo.pop()
+    return current_str
+
+
+methods = [append, delete, get, undo, redo]
+
+
+def BastShoe(cmd: str) -> str:
+
+    method = methods[int(cmd[0]) - 1]
+    return method(cmd[2:])
