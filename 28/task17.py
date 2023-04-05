1b6e31f265fe304acad891e4713c19343ce685e3
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2023-04-05 12:53:24 +0300

    correct mistakes

--- 28/task17.py
+++ 28/task17.py
@@ -13,7 +13,7 @@ def TreeOfLife(H: int, W: int, N: int, tree: List[str]) -> List[str]:
     Returns:
         List[str]: The final state of the tree after N years as a list of strings with "." and "+" characters.
     """
-    def increment_ages(tree: List[List[int]]):
+    def increment_ages(int_tree: List[List[int]]) -> None:
         """
         Increment the age of each cell in the tree by 1.
         
@@ -22,28 +22,9 @@ def TreeOfLife(H: int, W: int, N: int, tree: List[str]) -> List[str]:
         """
         for row in range(H):
             for col in range(W):
-                tree[row][col] += 1
-                
-    def kill_trees(tree: List[List[int]]):
-        """
-        Remove tree branches that are 3 or more years old, along with their neighbors.
-        
-        Args:
-            tree (List[List[int]]): The current tree state as a list of lists with integer values.
-        """
-        to_kill = []
-        for row in range(H):
-            for col in range(W):
-                if tree[row][col] >= 3:
-                    to_kill.append((row, col))
-                    for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-                        r, c = row + dr, col + dc
-                        if 0 <= r < H and 0 <= c < W:
-                            to_kill.append((r, c))
-        for r, c in to_kill:
-            tree[r][c] = 0
+                int_tree[row][col] += 1
 
-    def add_new_branches(tree: List[List[int]]):
+    def add_new_branches(int_tree: List[List[int]]) -> None:
         """
         Add new tree branches with an age of 1 in empty cells.
         
@@ -52,20 +33,33 @@ def TreeOfLife(H: int, W: int, N: int, tree: List[str]) -> List[str]:
         """
         for row in range(H):
             for col in range(W):
-                if tree[row][col] == 0:
-                    tree[row][col] = 1
-                    
-    def tree_to_str(tree: List[List[int]]) -> List[str]:
+                if int_tree[row][col] == 0:
+                    int_tree[row][col] = 1
+
+    def kill_trees(int_tree: List[List[int]]) -> None:
         """
-        Convert the tree with integer values to a list of strings with "." and "+" characters.
+        Remove tree branches that are 3 or more years old, along with their neighbors.
         
         Args:
             tree (List[List[int]]): The current tree state as a list of lists with integer values.
-            
-        Returns:
-            List[str]: The tree state as a list of strings with "." and "+" characters.
         """
-        return [''.join('+' if cell > 0 else '.' for cell in row) for row in tree]
+        to_kill = []
+
+        for row in range(H):
+            for col in range(W):
+                if int_tree[row][col] >= 3:
+                    to_kill.append((row, col))
+                    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                        r, c = row + dr, col + dc
+                        if 0 <= r < H and 0 <= c < W:
+                            to_kill.append((r, c))
+
+        for row, col in to_kill:
+            int_tree[row][col] = 0
+
+    def int_tree_to_str_list(int_tree: List[List[int]]) -> List[str]:
+        """Convert the integer tree representation to a list of strings with '+' and '.' symbols."""
+        return [''.join('+' if cell > 0 else '.' for cell in row) for row in int_tree]
 
     int_tree = [[1 if cell == '+' else 0 for cell in row] for row in tree]
 
@@ -73,7 +67,6 @@ def TreeOfLife(H: int, W: int, N: int, tree: List[str]) -> List[str]:
         increment_ages(int_tree)
         if year % 2 == 0:
             add_new_branches(int_tree)
-        else:
-            kill_trees(int_tree)
-            
-    return tree_to_str(int_tree)
+        kill_trees(int_tree)
+
+    return int_tree_to_str_list(int_tree)
