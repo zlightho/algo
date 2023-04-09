84bf8626dc40648284a0126df7e9117f4bd9d615
Михайлов Артем Константинович <Mikhaylov.A.K@sblogistica.ru>
2023-04-09 10:52:55 +0300

    task19.py

---
+++ 28/task19.py
@@ -0,0 +1,42 @@
+from typing import List
+
+def transform(arr: List[int]) -> List[int]:
+    """
+    Perform a transformation on the input array as per the given algorithm.
+
+    :param arr: A list of positive integers.
+    :return: A list of integers after applying the transformation.
+
+    The transformation algorithm consists of the following steps:
+    1. Initialize an empty list called 'result'.
+    2. Loop through the elements of the input array 'arr' with two nested loops.
+    3. Calculate the value of 'k' as the sum of the loop indices 'i' and 'j'.
+    4. Extract a subarray from 'arr' starting from index 'j' to index 'k' (inclusive).
+    5. Find the maximum value in the subarray and append it to the 'result' list.
+    """
+    result = []
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            k = i + j
+            result.append(max(arr[j:k+1]))
+    return result
+
+def TransformTransform(A: List[int], N: int) -> bool:
+    """
+    Calculate the key by applying the transform function twice and check if the key is even.
+
+    :param A: A list of positive integers.
+    :param N: The length of the input list A.
+    :return: True if the sum of all values in the result of the double transformation is even, False otherwise.
+
+    The function performs the following steps:
+    1. Apply the 'transform' function to the input list 'A' to obtain the first transformation result.
+    2. Apply the 'transform' function to the first transformation result to obtain the second transformation result.
+    3. Calculate the key as the sum of all elements in the second transformation result.
+    4. Check if the key is even and return True if it is, otherwise return False.
+    """
+    B = transform(A)
+    B = transform(B)
+    key = sum(B)
+    return key % 2 == 0
